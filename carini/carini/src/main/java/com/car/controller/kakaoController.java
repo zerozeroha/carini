2c3b3a9eab113fecf8f78652d7b639f60cf64d7e
nayunho <ring147@naver.com>
2024-05-30 11:26:03 +0900

    test

--- carini/carini/src/main/java/com/car/controller/kakaoController.java
+++ carini/carini/src/main/java/com/car/controller/kakaoController.java
@@ -1,8 +1,14 @@
 package com.car.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class kakaoController {
 	
+	@Value("${naver.client-id}")
+    private String clientId;
+
+    @Value("${naver.redirect-uri}")
+    private String redirectUri;
 }
