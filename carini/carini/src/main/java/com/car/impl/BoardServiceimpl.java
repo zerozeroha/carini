affc30f75c9475551e6c9523b5698caabc116ffa
nayunho <ring147@naver.com>
2024-06-03 23:59:43 +0900

    통합

--- carini/carini/src/main/java/com/car/impl/BoardServiceimpl.java
+++ carini/carini/src/main/java/com/car/impl/BoardServiceimpl.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.car.dto.Board;
 import com.car.dto.Car;
 import com.car.dto.Member;
@@ -29,6 +32,52 @@ public class BoardServiceimpl implements BoardService{
 		}else {
 			return boardList;
 		}
+	}
+
+	@Override
+	public long getTotalRowCount(Board board) {
+		return boardRepository.count();
+	}
+
+	@Override
+	public Board getBoard(Board board) {
+		Optional<Board> findBoard = boardRepository.findById(board.getBoardId());
+		if(findBoard.isPresent()) {
+			boardRepository.updateReadCount(board.getBoardId());
+			return findBoard.get();
+		} else {
+			return null;			
+		}
+	}
+
+	@Override
+	public Page<Board> getBoardList(Pageable pageable, String searchType, String searchWord) {
+		if(searchType.equalsIgnoreCase("board_title")) {
+			return boardRepository.findByBoardTitleContaining(searchWord, pageable);
+		} else if(searchType.equalsIgnoreCase("board_writer")) {
+			return boardRepository.findByBoardWriterContaining(searchWord, pageable);
+		} else {
+			return boardRepository.findByBoardContentContaining(searchWord, pageable);
+		}
+	}
+
+	@Override
+	public void insertBoard(Board board) {
+		boardRepository.save(board);
+//		boardRepository.updateLastSeq(0L, 0L, board.getBoardId());
+	}
+
+	@Override
+	public void updateBoard(Board board) {
+		Board findBoard = boardRepository.findById(board.getBoardId()).get();
+		findBoard.setBoardTitle(board.getBoardTitle());
+		findBoard.setBoardContent(board.getBoardContent());
+		boardRepository.save(findBoard);
+	}
+
+	@Override
+	public void deleteBoard(Board board) {
+		boardRepository.deleteById(board.getBoardId());
 	}
 
 	
