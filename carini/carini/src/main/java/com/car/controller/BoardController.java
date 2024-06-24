7cf5f9ad80f3377d1a1501dd882f3ac4591ad170
zerozeroha <158129874+zerozeroha@users.noreply.github.com>
2024-06-24 17:51:17 +0900

    중간통합(하영0624) (#85)
    * 하영 중간통합 (0613 / 1440)
    
    * 중간통합(하영)
    
    * 통합
    
    * 중간통합(하영)
    
    ---------
    
    Co-authored-by: nayunho <90189209+nayunho@users.noreply.github.com>

--- carini/carini/src/main/java/com/car/controller/BoardController.java
+++ carini/carini/src/main/java/com/car/controller/BoardController.java
@@ -197,7 +197,9 @@ public class BoardController {
 			return "board/insertBoard";
       }
 
+
       // 파일업로드
+
       MultipartFile uploadFile = board.getUploadFile();
       if(!uploadFile.isEmpty()) {
          String fileName = uploadFile.getOriginalFilename();
